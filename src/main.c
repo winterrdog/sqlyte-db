b62bdf7c22ac9bb6d19c606cd4a3f66571f35a9d
winterrdog <winterrdog@protonmail.ch>
2024-05-11 17:00:28 +0300

    chore: fixed command line argument check to be more specific

--- src/main.c
+++ src/main.c
@@ -370,7 +370,7 @@ void run_repl(const char* fname)
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2) {
+    if (argc != 2) {
         printf("you must supply a database filename.\n");
         return EXIT_FAILURE;
     }
