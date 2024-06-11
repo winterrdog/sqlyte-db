a792a9f0ede930dfb39df09e74d840adcf7d7c61
winterrdog <winterrdog@protonmail.ch>
2024-06-11 13:02:56 +0300

    fix: added support for compiling with `clang`

--- compile.sh
+++ compile.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 echo "+ compiling the source code..."
+
+# check if gcc is installed
+if ! [ -x "$(command -v gcc)" ]; then
+    echo -e "- error: gcc is not installed.\n" >&2
+    echo "+ falling back to clang compiler..."
+    echo "+ using clang to compile the source code..."
+    
+    clang -Ofast -o sqlyte src/*.c
+    
+    exit 1
+fi
+
+echo "+ using gcc to compile the source code..."
 gcc -Ofast -o sqlyte src/*.c
